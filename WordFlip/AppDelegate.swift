31cb86d0f069a7e1b8aa894edb53820b85a234d0
vityha20022 <vityha20022@mail.ru>
2024-03-20 14:50:27 +0300

    Fix swiftlint warnings

--- WordFlip/AppDelegate.swift
+++ WordFlip/AppDelegate.swift
@@ -10,7 +10,6 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -33,6 +32,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
 
 }
