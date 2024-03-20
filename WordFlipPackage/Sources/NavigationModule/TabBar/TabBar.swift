f35a1244137d39b03b248ef830038cab04b83821
PaulDotSimon <simonenko.0418@gmail.com>
2024-03-21 02:05:00 +0300

    Create folder Navigation Module and added new file for Main View

---
+++ WordFlipPackage/Sources/NavigationModule/TabBar/TabBar.swift
@@ -0,0 +1,27 @@
+import UIKit
+import WordFlipPackage
+
+public class TabBarViewController: UITabBarController {
+    
+    private let myTabBar = TabBar()
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        setValue(myTabBar, forKey: "tabBar")
+        setupTapItems()
+    }
+    
+    private func setupTapItems() {
+        let firstVC = ViewController()
+        firstVC.tabBarItem.title = "first"
+        firstVC.tabBarItem.image = UIImage(systemName: "graduationcap.fill")
+        let secondVC = ViewController()
+        secondVC.tabBarItem.title = "second"
+        secondVC.tabBarItem.image = UIImage(systemName: "doc.questionmark.fill.rtl")
+        let thridVC = ViewController()
+        thridVC.tabBarItem.title = "thrid"
+        thridVC.tabBarItem.image = UIImage(systemName: "person.fill")
+        
+        setViewControllers([firstVC, secondVC, thridVC], animated: false)
+    }
+}
