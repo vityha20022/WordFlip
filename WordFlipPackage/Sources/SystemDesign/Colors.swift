bf8c6f8cd92fe1c9d8201e6ce1dabc3f54cbb276
Victor Borisovsky <vyuborisovskiy@stud.eltech.ru>
2024-03-23 00:25:37 +0300

    Update system design

--- WordFlipPackage/Sources/SystemDesign/Colors.swift
+++ WordFlipPackage/Sources/SystemDesign/Colors.swift
@@ -2,13 +2,17 @@
 import UIKit
 
 public enum BaseColorScheme: CaseIterable {
-    case accent, buttonText
-    func resolve() -> UIColor {
+    case accent, buttonText, backgroundColor, shadowColor
+    public func resolve() -> UIColor {
         switch self {
             case .accent:
                 return UIColor(named: "AccentColor")!
             case .buttonText:
                 return UIColor(named: "ButtonTextColor")!
+            case .backgroundColor:
+                return UIColor(named: "BackgroundColor")!
+            case .shadowColor:
+                return UIColor(named: "ShadowColor")!
         }
     }
 }
