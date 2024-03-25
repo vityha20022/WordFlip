c16123b56beb495a29211c0a3c63db59af2e3fb1
vityha20022 <vityha20022@mail.ru>
2024-03-25 16:42:29 +0300

    Add front/down side card colors

--- WordFlipPackage/Sources/SystemDesign/Colors.swift
+++ WordFlipPackage/Sources/SystemDesign/Colors.swift
@@ -9,25 +9,31 @@ public enum BaseColorScheme: CaseIterable {
     case backgroundColor
     case shadowColor
     case black
+    case frontSideCardColor
+    case downSideCardColor
   
     public func resolve() -> UIColor {
         switch self {
-        case .accent:
-            return UIColor(named: "AccentColor")!
-        case .baseTint:
-            return UIColor(named: "BaseTintColor")!
-        case .cardText:
-            return UIColor(named: "CardTextColor")!
-        case .buttonText:
-            return UIColor(named: "ButtonTextColor")!
-        case .red:
-            return UIColor(named: "RedColor")!
-        case .backgroundColor:
-            return UIColor(named: "BackgroundColor")!
-        case .shadowColor:
-            return UIColor(named: "ShadowColor")!
-        case .black:
-            return UIColor(named: "TextColor")!
+            case .accent:
+                return UIColor(named: "AccentColor")!
+            case .baseTint:
+                return UIColor(named: "BaseTintColor")!
+            case .cardText:
+                return UIColor(named: "CardTextColor")!
+            case .buttonText:
+                return UIColor(named: "ButtonTextColor")!
+            case .red:
+                return UIColor(named: "RedColor")!
+            case .backgroundColor:
+                return UIColor(named: "BackgroundColor")!
+            case .shadowColor:
+                return UIColor(named: "ShadowColor")!
+            case .black:
+                return UIColor(named: "TextColor")!
+            case .frontSideCardColor:
+                return UIColor(named: "FrontSideCardColor")!
+            case .downSideCardColor:
+                return UIColor(named: "DownSideCardColor")!
         }
     }
 }
