f407be3fa58d4130514f4fa4357d28cc7f2f3c09
br_zahar <br_zahar@mail.ru>
2024-03-26 01:01:46 +0300

    Implement protocol for ProfileViewController

--- WordFlipPackage/Sources/ProfilePackage/ViewController/ProfileViewController.swift
+++ WordFlipPackage/Sources/ProfilePackage/ViewController/ProfileViewController.swift
@@ -7,10 +7,20 @@ public final class ProfileViewController: UIViewController {
     private let exitButton = DefaultButton(text: "Exit", color: BaseColorScheme.red)
     private let deleteButton = UIButton()
     private let titleLabel = UILabel()
+    private let presenter: ProfilePresenterProtocol
+    
+    init(presenter: ProfilePresenterProtocol) {
+        self.presenter = presenter
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     public override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         setupLayout()
     }
     
@@ -90,7 +100,7 @@ public final class ProfileViewController: UIViewController {
 
 extension ProfileViewController: UITableViewDataSource {
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return presenter.numberRowsInSection(section: section)
     }
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -103,7 +113,7 @@ extension ProfileViewController: UITableViewDataSource {
             } else {
                 cell = UserTableViewCell(style: .default, reuseIdentifier: "UserCell")
             }
-            
+
         } else {
             
             if let reuseCell = tableView.dequeueReusableCell(withIdentifier: "DefaultCell") as? DefaultTableViewCell {
@@ -120,20 +130,45 @@ extension ProfileViewController: UITableViewDataSource {
     }
     
     private func configureCell(cell: CustomCellProtocoll, for indexPath: IndexPath) {
-        if indexPath.row == 0 {
-            cell.configure(image: UIImage(systemName: "person.crop.circle"), text: "br_zahar", isOn: false)
-        } else if indexPath.row == 1 {
-            cell.configure(image: UIImage(systemName: "gear"), text: "Settings", isOn: false)
-        } else if indexPath.row == 2 {
-            cell.configure(image: UIImage(systemName: "info.circle"), text: "About the program", isOn: false)
-        } else {
-            cell.configure(image: UIImage(systemName: "exclamationmark.triangle"), text: "default", isOn: false)
-        }
-        
+        let model = presenter.getDataArray()[indexPath.row]
+        cell.configure(image: model.image, text: model.labelText, isOn: model.isOn, closureForAction: model.closureForAction)
         cell.selectionStyle = .none
     }
     
 }
 
 extension ProfileViewController: UITableViewDelegate {
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        presenter.didTapSettings(cellIndex: indexPath.row)
+    }
+}
+
+extension ProfileViewController: ProfileViewProtocol {
+    func showSettingsScreen() {
+        navigationController?.pushViewController(SettingsBuilder().build(), animated: true)
+    }
+    
+    func showAppInfoAlert() {
+        let alertController = UIAlertController(
+            title: "О приложении",
+            message: """
+            "WordFlip" - это приложение для изучения языков с помощью карточек. Это эффективный и удобный способ улучшить свои навыки, независимо от вашего уровня владения языком. Наше приложение поможет достичь ваших языковых целей, обогатить словарный запас и повысить вашу уверенность в общении на новом языке.
+
+            Разработчики:
+                - Борисовский Виктор
+                - Брюханов Захар Дмитриевич
+                - Кисляков Никита Александрович
+                - Симоненко Павел Александрович
+            
+            Отдельная благодарность:
+                - Сницарюк Роман
+            """,
+            preferredStyle: .alert
+        )
+        
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
 }
