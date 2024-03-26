a48453ecd58c0ad022550f3afe027e8de5c32ab1
vityha20022 <vityha20022@mail.ru>
2024-03-27 00:20:02 +0300

    Add UI for language buttons in CardRedactorViewController

--- WordFlipPackage/Sources/EntityModule/Card/CardRedactorViewController.swift
+++ WordFlipPackage/Sources/EntityModule/Card/CardRedactorViewController.swift
@@ -21,6 +21,7 @@ public struct CardRedactorConfiguration {
 
 final public class CardRedactorViewController: UIViewController {
     private let configuration: CardRedactorConfiguration
+    private let languageData = ["English", "Russian", "Germany"]
     
     private let headerLabel: UILabel = {
         let label = UILabel()
@@ -43,6 +44,62 @@ final public class CardRedactorViewController: UIViewController {
         button.configuration = .tinted()
         button.setTitle("Translate", for: .normal)
         return button
+    }()
+    
+    private lazy var sourceLanguageButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.showsMenuAsPrimaryAction = true
+        button.titleLabel?.font = .systemFont(ofSize: 20)
+        button.setTitleColor(BaseColorScheme.accent.resolve(), for: .normal)
+        button.backgroundColor = .systemGray5
+        button.layer.cornerRadius = 7
+        
+        var menuActions = [UIAction]()
+        
+        for language in languageData {
+            let action = UIAction(title: "\(language)", handler: { [self] _ in
+                button.setTitle("\(language)", for: .normal)
+            })
+            menuActions.append(action)
+        }
+        
+        let menu = UIMenu(title: "Source language:", children: menuActions)
+        button.menu = menu
+        button.setTitle(languageData[0], for: .normal)
+        
+        return button
+    }()
+    
+    private lazy var targetLanguageButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.showsMenuAsPrimaryAction = true
+        button.titleLabel?.font = .systemFont(ofSize: 20)
+        button.setTitleColor(BaseColorScheme.accent.resolve(), for: .normal)
+        button.backgroundColor = .systemGray5
+        button.layer.cornerRadius = 7
+        
+        var menuActions = [UIAction]()
+        
+        for language in languageData {
+            let action = UIAction(title: "\(language)", handler: { [self] _ in
+                button.setTitle("\(language)", for: .normal)
+            })
+            menuActions.append(action)
+        }
+        
+        let menu = UIMenu(title: "Target language:", children: menuActions)
+        button.menu = menu
+        button.setTitle(languageData[1], for: .normal)
+        
+        return button
+    }()
+    
+    private let arrowImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(systemName: "arrow.forward"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
     }()
     
     public init(with configuration: CardRedactorConfiguration) {
@@ -78,15 +135,32 @@ final public class CardRedactorViewController: UIViewController {
         view.addSubview(frontSideCardView)
         view.addSubview(translateButton)
         view.addSubview(downSideCardView)
+        view.addSubview(sourceLanguageButton)
+        view.addSubview(arrowImageView)
+        view.addSubview(targetLanguageButton)
         
         NSLayoutConstraint.activate([
-            headerLabel.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 50),
+            headerLabel.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: ComponentMetrics.headerTitleTopMargin),
             headerLabel.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 10),
             
             translateButton.centerYAnchor.constraint(equalTo: headerLabel.centerYAnchor),
             translateButton.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -10),
             
-            frontSideCardView.topAnchor.constraint(equalTo: headerLabel.bottomAnchor, constant: 10),
+            sourceLanguageButton.topAnchor.constraint(equalTo: headerLabel.bottomAnchor, constant: 20),
+            sourceLanguageButton.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 10),
+            sourceLanguageButton.trailingAnchor.constraint(equalTo: arrowImageView.leadingAnchor, constant: -5),
+            
+            
+            arrowImageView.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor),
+            arrowImageView.widthAnchor.constraint(equalToConstant: 30),
+            arrowImageView.heightAnchor.constraint(equalToConstant: 30),
+            arrowImageView.centerYAnchor.constraint(equalTo: targetLanguageButton.centerYAnchor),
+            
+            targetLanguageButton.topAnchor.constraint(equalTo: headerLabel.bottomAnchor, constant: 20),
+            targetLanguageButton.leadingAnchor.constraint(equalTo: arrowImageView.trailingAnchor, constant: 5),
+            targetLanguageButton.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -10),
+            
+            frontSideCardView.topAnchor.constraint(equalTo: sourceLanguageButton.bottomAnchor, constant: 20),
             frontSideCardView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 10),
             frontSideCardView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -10),
             frontSideCardView.bottomAnchor.constraint(equalTo: downSideCardView.topAnchor, constant: -10),
