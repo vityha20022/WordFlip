a14d34b0c41a7dc76b2f61491626d0b618acf86d
vityha20022 <vityha20022@mail.ru>
2024-03-27 20:19:57 +0300

    Add headerTitleTopMarginWithNavBar constant in ComponentMetrics

--- WordFlipPackage/Sources/SystemDesign/ComponentMetrics.swift
+++ WordFlipPackage/Sources/SystemDesign/ComponentMetrics.swift
@@ -5,4 +5,5 @@ public enum ComponentMetrics {
     public static let shadowRadius: CGFloat = 3.0
     public static let shadowOffset = CGSize(width: 5.0, height: 5.0)
     public static let headerTitleTopMargin: CGFloat = 20
+    public static let headerTitleTopMarginWithNavBar: CGFloat = 10
 }
